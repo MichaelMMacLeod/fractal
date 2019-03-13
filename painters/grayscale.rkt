cb0e100ec46739b5476325ea0a3ffe86a8b44815
Michael MacLeod <michaelmmacleod@gmail.com>
2019-03-12 21:49:23 -0700

    remove painters

--- painters/grayscale.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-(require "painter.rkt")
-
-(define-painter (grayscale [iterations exact-nonnegative-integer?])
-  (define v (cond [(> iterations 255)
-                   (- 255 (modulo iterations 255))]
-                  [else iterations]))
-  (argb-color 255 v v v))
